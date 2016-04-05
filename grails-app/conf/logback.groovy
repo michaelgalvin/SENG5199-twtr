2b3a817a7f08a6517186755af73ca2eb154acb9e
Magdalena Cruz Ramirez <cruzr004@Magdalena.local>
2016-04-04 21:36:37 -0500

    Mike : This is what i have since last night after i merged your changes.:

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -30,28 +30,28 @@ if (Environment.isDevelopmentMode() && targetDir) {
 //
 //// See http://logback.qos.ch/manual/groovy.html for details on configuration
 
-import grails.util.BuildSettings
-import grails.util.Environment
-
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
-appender('STDOUT', ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
-    }
-}
-
-root(ERROR, ['STDOUT'])
-
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
-        }
-    }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], true)
-    logger("org.springframework.security", DEBUG, ['STDOUT'], true)
-    logger("grails.plugin.springsecurity", DEBUG, ['STDOUT'], true)
-}
+//import grails.util.BuildSettings
+//import grails.util.Environment
+//
+//// See http://logback.qos.ch/manual/groovy.html for details on configuration
+//appender('STDOUT', ConsoleAppender) {
+//    encoder(PatternLayoutEncoder) {
+//        pattern = "%level %logger - %msg%n"
+//    }
+//}
+//
+//root(ERROR, ['STDOUT'])
+//
+//def targetDir = BuildSettings.TARGET_DIR
+//if (Environment.isDevelopmentMode() && targetDir) {
+//    appender("FULL_STACKTRACE", FileAppender) {
+//        file = "${targetDir}/stacktrace.log"
+//        append = true
+//        encoder(PatternLayoutEncoder) {
+//            pattern = "%level %logger - %msg%n"
+//        }
+//    }
+//    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], true)
+//    logger("org.springframework.security", DEBUG, ['STDOUT'], true)
+//    logger("grails.plugin.springsecurity", DEBUG, ['STDOUT'], true)
+//}
