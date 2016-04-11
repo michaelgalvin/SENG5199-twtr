a17745d3c87002d37c64a7c0a3c508b1b4911477
Magdalena Cruz Ramirez <cruzr004@Magdalena.local>
2016-04-10 23:21:36 -0500

    user detail shows messages; start of following logic

--- grails-app/assets/javascripts/app/controllers/UserDetailCtrl.js
+++ grails-app/assets/javascripts/app/controllers/UserDetailCtrl.js
@@ -1,8 +1,29 @@
 /**
  * Created by cruzr004 on 4/10/16.
  */
-angular.module('app').controller('UserDetailCtrl', ['$scope', '$location', '$resource', function ($scope, $location, $resource) {
+angular.module('app').controller('UserDetailCtrl', ['$scope', '$location', '$resource','securityService', function ($scope, $location, $resource, securityService) {
+
+    var handleQuery = $location.$$search.q;
+    var currentHandle = securityService.currentUser().handle;
 
     var UserDetailQuery = $resource('/api/account/show/:query');
-    $scope.user = UserDetailQuery.get({query: $location.$$search.q});
+    var loggedInUser = UserDetailQuery.get({query: currentHandle});
+
+    // need to
+    // 1) verify that there are followers and
+    // 2) if there are, loop through and see if currentUser is one of them
+    var FollowersQuery = $resource('/api/account/following/:query', {query:loggedInUser.Id});
+    var followers  = FollowersQuery.query(); //get({handle:$location.$$search.q});
+    //http://localhost:8080/api/account/following/1
+
+    // 3) change the flag to true if I am following
+    // 4) if false then clicking on it should make the loggedInUser follow the
+    //    user displayed on the user detail page
+    $scope.iAmFollowing = false;
+
+    $scope.loggedInUser = loggedInUser;
+    $scope.user = UserDetailQuery.get({query: handleQuery});
+
+    var MessageQuery = $resource('/api/message/accnt/:query', {query:handleQuery});
+    $scope.messages  = MessageQuery.query(); //get({handle:$location.$$search.q});
 }]);
