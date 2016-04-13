91aa315d9dc833e0ed8710bd8d07ec9ab594d76d
Magdalena Cruz Ramirez <cruzr004@Magdalena.local>
2016-04-12 20:40:41 -0500

    new bootstrap with new messages

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -11,9 +11,9 @@ class BootStrap {
         new AccountRole(user: admin, role: role).save(flush: true, failOnError: true)
 
         // Define some intial users
-        def test1 = new Account(handle: 'groovyNewbie', email: 'newb@gmail.com', password: '12345678aH', name: 'Mike').save()
+        def test1 = new Account(handle: 'groovyNewbie', email: 'newb@gmail.com', password: '12345678aH', name: 'Mike',followers:[test2], following: [test2]).save()
         new AccountRole(user: test1, role: role).save(flush: true, failOnError: true)
-        def  test2 = new Account(handle: 'groovyNewbie2', email: 'newby2@gmail.com', password: '12345678aH', name: 'Newb').save()
+        def  test2 = new Account(handle: 'groovyNewbie2', email: 'newby2@gmail.com', password: '12345678aH', name: 'Newb',followers:[test1], following: [test1]).save()
         new AccountRole(user: test2, role: role).save(flush: true, failOnError: true)
         def test3 = new Account(handle: 'PossessWeekly2', email: 'finallyPossessed@gmail.com',password: '12345678aH', name: 'Toro').save()
         new AccountRole(user: test3, role: role).save(flush: true, failOnError: true)
@@ -24,39 +24,43 @@ class BootStrap {
         def test6 = new Account(handle: 'Nerd2', email: '234hn@gmail.com',password: '12345678aH', name: 'totoro').save()
         new AccountRole(user: test6, role: role).save(flush: true, failOnError: true)
 
-        for (int i=1; i<14;i++) {
-            def message = new Message()
-            message.author = test1
-            message.text = 'Hello ' + i
-            message.save()
-        }
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 1').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 2').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 3').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 4').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 5').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 6').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 7').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 8').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 9').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 10').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 11').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 12').save()
-//        new seng5199.twtr.Message(author: test1, text: 'Hello 13').save()
 
-        new seng5199.twtr.Message(author: 2, text: 'Hi 1').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 2').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 3').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 4').save()
-        new seng5199.twtr.Message(author: 2, text: 'abc defghigklmnop').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 6').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 7').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 8').save()
-        new seng5199.twtr.Message(author: 2, text: 'Save the date 9').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 10').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 11').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 12').save()
-        new seng5199.twtr.Message(author: 2, text: 'Hi 13').save()
+
+
+
+        new seng5199.twtr.Message(author: test1, text: 'Hello 1').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 2').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 3').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 4').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 5').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 6').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 7').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 8').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 9').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 10').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 11').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 12').save()
+        new seng5199.twtr.Message(author: test1, text: 'Hello 13').save()
+
+        new seng5199.twtr.Message(author: test2, text: 'Just remember, everything happens for a reason').save()
+        new seng5199.twtr.Message(author: test2, text: 'Hi 2').save()
+        new seng5199.twtr.Message(author: test2, text: 'Um I really really like cookies').save()
+        new seng5199.twtr.Message(author: test2, text: 'Hi 4').save()
+        new seng5199.twtr.Message(author: test2, text: 'The boys in my class always say that... my answer is potato').save()
+        new seng5199.twtr.Message(author: test2, text: 'Hi 6').save()
+        new seng5199.twtr.Message(author: test2, text: 'Hi 7').save()
+        new seng5199.twtr.Message(author: test2, text: 'My favorite color of the alphabet is fries').save()
+        new seng5199.twtr.Message(author: test2, text: 'Save the date 9').save()
+        new seng5199.twtr.Message(author: test2, text: 'This is just flat out hilarious').save()
+        new seng5199.twtr.Message(author: test2, text: 'Pshhhh I did not fall... The floor looked at me funny \n' +
+                'so I used my mad ninja skills to attack').save()
+        new seng5199.twtr.Message(author: test2, text: 'Hi 12').save()
+        new seng5199.twtr.Message(author: test2, text: 'Hi 13').save()
+
+
+
+
+
     }
     def destroy = {
 
