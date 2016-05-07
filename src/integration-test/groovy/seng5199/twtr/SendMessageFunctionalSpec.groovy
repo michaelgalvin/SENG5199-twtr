b775831809051f7bf511ead87442c37ee6070e1e
magdalenacruz <cruzr004@umn.edu>
2016-05-07 14:11:57 -0500

    Finally got it done

--- src/integration-test/groovy/seng5199/twtr/SendMessageFunctionalSpec.groovy
+++ src/integration-test/groovy/seng5199/twtr/SendMessageFunctionalSpec.groovy
@@ -14,25 +14,48 @@ class SendMessageFunctionalSpec extends GebSpec {
         sleep(1000)//Breaks without this. Not sure why.
     }
 
-    def 'R2. Use Angular validation to validate a message prior to posting it to the server via the REST API (client side validation).'() {
+    def 'R1. Use a alert control from the Angular UI library to display an info message saying Message Posted!'() {
         when:
         $('#send-message-link')[0].click();
 
         then:
-        waitFor{ $('#twtr-text') }
-        $("#twtr-text").value("Dogs have masters. Cats have staff.")
+        waitFor { $('#twtr-text') }
+        $("#twtr-text").value("GOOG NEWS we are done with the WEB class..lets celebrate!.")
 
         when:
         $('#twtr-check').click()//Click the checkbox
         $('#message-button')[0].click();//Click the button
 
         then:
-        waitFor { $('#message-alert').text() == "Message Posted!" }
+        waitFor {$('#message-alert').text() == "Message Posted!"
+        }
 
-        when:
-        $("#twtr-text").value("This message is waaaaaaaaaaaayyyyyyyyyyy toooooooooooooo loooooooooooonnnnnngggg.")
-
-        then:
-        waitFor { $('#message-alert').text() == "Your tweet can be only 40 characters long." }
     }
+def 'R2. Use Angular validation to validate a message prior to posting it to the server via the REST API (client side validation).'() {
+
+    when:
+    $('#send-message-link')[0].click();
+
+    then:
+    waitFor{ $('#twtr-text') }
+    $("#twtr-text").value("Dogs have masters. Cats have staff.")
+
+    when:
+    //$('#twtr-check').click()//Click the checkbox
+    $('#message-button')[0].click();//Click the button
+
+    then:
+    waitFor { $('#message-alert').text() == "Only humans can tweet using this application!" }
+
+    when:
+    $('#twtr-check').click()//Click the checkbox
+    $("#twtr-text").value("OK need to check if I was a human ..ja ja - lets celebrate!.")
+    $('#message-button')[0].click();//Click the button
+
+    then:
+    waitFor { $('#message-alert').text() == "Message Posted!" }
+}
+
+
+
 }
--- src/test/javascript/messageControllerSpec.js
+++ src/test/javascript/messageControllerSpec.js
@@ -1,8 +1,65 @@
-describe("true", function()
-{
-    it("Should be true", function(){
-        expect(true).toBeTruthy()
+//describe("true", function() {
+//    it("Should be true", function(){
+//        expect(false).toBeTruthy()
+//
+//    });
+//
+//});
+
+
+describe("messageController", function () {
+    beforeEach(module('app'));
+
+    var $httpBackend, $scope, $controller, securityService;
+    var currentUser = undefined;
+    var gn = '{"username": "groovyNewbie","roles": ["ROLE_READ"],"access_token": "ldlggjvnnktbhv06s810tr7ss9b26m4l"}';
+    var securityService = {
+        currentUser: function () {
+            return currentUser;
+        },
+
+        setCurrentUser: function () {
+            return currentUser;
+        },
+
+        loginSucess: function () {
+            return gn;
+        }
+    };
+
+
+    beforeEach(inject(function ($injector) {
+        $controller = $injector.get('$controller');
+        $httpBackend = $injector.get('$httpBackend');
+        // securityService = $injector.get('securityService');
+        $scope = $injector.get('$rootScope').$new();
+
+    }));
+    describe("Message Posting", function () {
+
+        beforeEach((function () {
+            currentUser = {token: 'xyz', role: 'admin', username: 'eddie'};
+            spyOn(securityService, 'currentUser').and.returnValue(currentUser);
+            $scope.twtrform = {twtrcheck: {$valid: true}};
+            $controller('messageController', {$scope: $scope, securityService: securityService});
+
+        }));
+
+        it('should post message', function () {
+
+            $httpBackend.when('POST', '/api/message', {
+                message: {
+                    text: "Tweeting from Karma",
+                    author: {handle: "eddie"}
+                }
+            }).respond(201, {author: {handle: 'eddie'}});
+            $scope.text = 'Tweeting from Karma';
+            $scope.postMessage();
+            $httpBackend.flush();
+        });
 
     });
 
 });
+
+
